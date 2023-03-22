81e954a440def15a4561f3e209fce77ebd23e735
kcho760 <kcho760@gmail.com>
2023-03-22 11:54:45 -0400

    fixing name

--- w3d3 homewor/recursion.rb
+++ w3d3 homewor/recursion.rb
@@ -54,3 +54,64 @@ def reverse(string)
 
     string[-1] + reverse(string[0..-2])
 end
+
+
+def fibs(n)
+    if n == 0
+        return []
+    end
+
+    if n == 1
+        return [0]
+    end
+
+    if n == 2
+        return [0,1]
+    end
+
+    return fibs(n-1) << fibs(n-1)[1]+ fibs(n-1)[-2]
+
+end
+
+def fast_fibs(n)
+    if n == 0
+        return []
+    end
+
+    if n == 1
+        return [0]
+    end
+
+    if n == 2
+        return [0,1]
+    end
+    prev-fibs = fast_fibs(n-1)
+    return prev-fibs << prev-fibs[1]+prev-fibs[-2]
+
+end
+
+def pascal_row(n)
+    if n == 0
+        return [1]
+    end
+
+    if n == 1
+        return [1,1]
+    end
+
+    last_row = pascal_row(n - 1) #pascal_row(3) == [1,2,1]
+    new_arr = []
+
+    last_row.each_with_index do |ele,idx|
+        break if idx == last_row.length - 1
+        new_arr << ele+last_row[idx+1]
+    end
+
+    pascal_row(0) + new_arr + pascal_row(0)
+
+end
+
+    #     1
+    #    1 1
+    #   1 2 1
+    #  1 3 3 1
