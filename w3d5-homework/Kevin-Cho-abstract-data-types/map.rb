a904302b2405c3bb6fa40a11fefb0d6bf0371cfa
kcho760 <kcho760@gmail.com>
2023-03-24 00:09:34 -0400

    Finished all Ruby files

--- w3d5-homework/Kevin-Cho-abstract-data-types/map.rb
+++ w3d5-homework/Kevin-Cho-abstract-data-types/map.rb
@@ -4,19 +4,27 @@ class Map
     end
 
     def set(key,value)
-
-
+        @i << [key,value]
     end
 
     def get(key)
+        @i.each do |pair|
+            if pair[0].include?(key)
+                return pair
+            end
+        end
 
     end
 
     def delete(key)
-
+        @i.each_with_index do |pair,idx|
+            if pair[0].include?(key)
+                @i.delete(pair)
+            end
+        end
     end
 
     def show
-
+        return @i[-1]
     end
 end
