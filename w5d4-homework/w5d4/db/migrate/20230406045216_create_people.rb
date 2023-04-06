d48b96876574c7e715b26258d67b7c77d85f323c
kcho760 <kcho760@gmail.com>
2023-04-06 01:21:20 -0400

    Added w5d4 hw

---
+++ w5d4-homework/w5d4/db/migrate/20230406045216_create_people.rb
@@ -0,0 +1,11 @@
+class CreatePeople < ActiveRecord::Migration[7.0]
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.integer :house_id
+
+      # validates :name, presence: true
+      t.timestamps
+    end
+  end
+end
---
+++ w5d4-homework/w5d4/db/migrate/20230406051213_add_associations_person.rb
@@ -0,0 +1,9 @@
+
+    class Person < ApplicationRecord
+      belongs_to(
+        :house,
+        class_name: 'house',
+        foreign_key: :house_id,
+        primary_key: :id
+      )
+  end
